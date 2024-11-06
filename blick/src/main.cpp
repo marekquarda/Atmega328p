270473f085b531dd22fd8950d7e57394c3c77d5f
marekquarda <mquarda@centrum.cz>
2024-11-06 16:27:11 +0100

    add green to blick

--- blick/src/main.cpp
+++ blick/src/main.cpp
@@ -4,14 +4,20 @@
 
 int main() 
 {
-    // init
+    // init 
+    // red led
     DDRD =  0b00000010;
     PORTD = 0b00000000;
+    // green led
+    DDRB =  0b00100000;
+    PORTB = 0b00000000;
 
     while(1) {
         PORTD = 0b00000010;
+        PORTB = 0b00000000;
         _delay_ms(500);
         PORTD = 0b00000000;
+        PORTB = 0b00100000;
         _delay_ms(500);
     }
 
