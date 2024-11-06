1ae7a89f317cef3169a8d29ca5f62ed8cd384d56
marekquarda <mquarda@centrum.cz>
2024-11-06 15:31:56 +0100

    blick 1s example

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,19 @@
 #include <avr/io.h>
+#include <util/delay.h>
 
-int main() {
+
+int main() 
+{
+    // init
+    DDRD =  0b00000010;
+    PORTD = 0b00000000;
+
+    while(1) {
+        PORTD = 0b00000010;
+        _delay_ms(500);
+        PORTD = 0b00000000;
+        _delay_ms(500);
+    }
+
     return 0;
 }
