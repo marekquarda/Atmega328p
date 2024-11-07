92c0b4c495dca8fbed22642723cccd9aed2be766
marekquarda <mquarda@centrum.cz>
2024-11-07 16:42:22 +0100

    dg

--- lcd/src/main.cpp
+++ lcd/src/main.cpp
@@ -14,10 +14,10 @@ int main(void)
 {
 	
 	lcd_init();
-	lcd_write_character('a');
-	lcd_write_character('h');
-	lcd_write_character('o');
-	lcd_write_character('j');
+	// lcd_write_character('a');
+	// lcd_write_character('h');
+	// lcd_write_character('o');
+	// lcd_write_character('j');
 	//lcd_write_word("Hello, World!");
 	//lcd_goto_xy(1,0);
 	//lcd_write_word("LCD is working");
