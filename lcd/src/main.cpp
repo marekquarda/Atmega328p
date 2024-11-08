55ae8b8e80906e04ebf35af1c31a13b58e1a8b24
marekquarda <mquarda@centrum.cz>
2024-11-08 23:10:12 +0100

    sadfs

--- lcd/src/main.cpp
+++ lcd/src/main.cpp
@@ -12,8 +12,8 @@ int main(void)
 {
 	lcd_init();
 	lcd_send_command(LCD_MOVE_CURSOR_RIGHT);
-	lcd_write_character('h');
-	lcd_write_character(1);
+	//lcd_write_character('h');
+	//lcd_write_character(1);
 	//lcd_write_character('j');
 	//lcd_write_word("Hello, World!");
 	//lcd_goto_xy(0,0);
