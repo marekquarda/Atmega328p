a753bd3389deec2bcc72a5140bdcbbf4f80d2a9b
marekquarda <mquarda@centrum.cz>
2024-11-08 22:59:46 +0100

    sd

--- lcd/src/main.cpp
+++ lcd/src/main.cpp
@@ -12,6 +12,10 @@ int main(void)
 {
 	lcd_init();
 	lcd_send_command(LCD_MOVE_CURSOR_RIGHT);
+	lcd_send_command(LCD_MOVE_CURSOR_RIGHT);
+	lcd_send_command(LCD_MOVE_CURSOR_RIGHT);
+	lcd_send_command(LCD_MOVE_CURSOR_RIGHT);
+	lcd_send_command(LCD_MOVE_CURSOR_RIGHT);
 	//lcd_write_character('h');
 	//lcd_write_character(1);
 	//lcd_write_character('j');
