56a738509d8b405b0d03fdcd11eec89b08acfa17
marekquarda <mquarda@centrum.cz>
2024-11-08 23:06:19 +0100

    asdfs

--- lcd/src/main.cpp
+++ lcd/src/main.cpp
@@ -16,10 +16,10 @@ int main(void)
 	lcd_send_command(LCD_MOVE_CURSOR_RIGHT);
 	lcd_send_command(LCD_MOVE_CURSOR_RIGHT);
 	lcd_send_command(LCD_MOVE_CURSOR_RIGHT);
-	//lcd_write_character('h');
-	//lcd_write_character(1);
+	lcd_write_character('h');
+	lcd_write_character(1);
 	//lcd_write_character('j');
-	lcd_write_word("Hello, World!");
+	//lcd_write_word("Hello, World!");
 	//lcd_goto_xy(0,0);
 	//lcd_write_character(1);
 	//lcd_write_word("LCD is working");
