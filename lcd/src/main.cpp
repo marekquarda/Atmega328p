a0bf29910bd518dd5e6501d260dcace11c6f58fc
marekquarda <mquarda@centrum.cz>
2024-11-09 16:04:19 +0100

    jakstak

--- lcd/src/main.cpp
+++ lcd/src/main.cpp
@@ -16,14 +16,5 @@ int main(void)
 	lcd_send_command(0xC0);		/* Go to 2nd line*/
 	lcd_write_word("Hello World");	/* Write string on 2nd line*/
 	while(1);
-	
-	//lcd_write_character('h');
-	//lcd_write_character(1);
-	//lcd_write_character('j');
-	//lcd_write_word("Hello, World!");
-	//lcd_goto_xy(1,1);
-	//lcd_write_character(1);
-	//lcd_write_word("LCD is working");
- 
 }
 
