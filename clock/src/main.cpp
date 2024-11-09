63d232b7a9a3443712a76ece8f7ca6dbfbcc75ea
marekquarda <mquarda@centrum.cz>
2024-11-10 00:41:01 +0100

    add clock

---
+++ clock/src/main.cpp
@@ -0,0 +1,48 @@
+/*
+ * main.c
+ *
+ * Created: 2024-11-01 20:00:32
+ * Author : Marek Xara Quarda
+ */ 
+#include "lcd.h"
+#include <avr/io.h>
+#include <util/delay.h>
+#include "clock.h"
+
+
+int main(void)
+{
+	lcd_init(); /* Initialization of LCD*/
+	//lcd_clear();
+	//lcd_write_word("ElectronicWINGS");	/* Write string on 1st line of LCD*/
+	//lcd_send_command(0xC0);		/* Go to 2nd line*/
+	//lcd_write_word("Hello World");	/* Write string on 2nd line*/
+	while(1) {
+		lcd_clear();
+		//lcd_write_word("Ahoj Marku.     ");
+		lcd_write_word("ABCDEFGHIJKLMNOP");
+		_delay_ms(2000);
+		lcd_clear();
+		lcd_write_word("QRSTWXYZ12345678");
+		//lcd_write_word("Jak se mas?     ");
+		_delay_ms(2000);
+		//lcd_write_word("ABCDEFGHIJKLMNOP");
+		//lcd_goto_xy(1,0);
+		//lcd_write_word("QRSTWXYZ12345678");
+		
+		// lcd_goto_xy(0,0);
+		// lcd_write_word("90!@#$%&*)@#@##@");
+		// lcd_goto_xy(1,0);
+		// lcd_write_word("ElectronicWINGS@");	/* Write string on 1st line of LCD*/
+		// //lcd_send_command(0xC0);		/* Go to 2nd line*/
+		// lcd_goto_xy(0,0);
+		// lcd_write_word("ElectronicWINGS@");	/* Write string on 2nd line*/
+		// lcd_clear();
+		// lcd_goto_xy(1,0);
+		// lcd_write_word("8888888888888888");	/* Write string on 1st line of LCD*/
+		// lcd_goto_xy(0,0);
+		// //lcd_send_command(0xC0);		/* Go to 2nd line*/
+		// lcd_write_word("8888888888888888");	/* Write string on 2nd line*/
+	};
+}
+
