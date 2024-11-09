e5ebff7b01a2740f0271e05c3da270c77382244d
marekquarda <mquarda@centrum.cz>
2024-11-09 21:31:41 +0100

    asdf

--- lcd/src/main.cpp
+++ lcd/src/main.cpp
@@ -20,10 +20,12 @@ int main(void)
 		lcd_goto_xy(1,0);
 		lcd_write_word("ElectronicWINGS@");	/* Write string on 1st line of LCD*/
 		//lcd_send_command(0xC0);		/* Go to 2nd line*/
+		lcd_goto_xy(0,0);
 		lcd_write_word("ElectronicWINGS@");	/* Write string on 2nd line*/
 		lcd_clear();
 		lcd_goto_xy(1,0);
 		lcd_write_word("8888888888888888");	/* Write string on 1st line of LCD*/
+		lcd_goto_xy(0,0);
 		//lcd_send_command(0xC0);		/* Go to 2nd line*/
 		lcd_write_word("8888888888888888");	/* Write string on 2nd line*/
 	};
