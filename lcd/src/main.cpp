29f7c4a1c9cf59826d8c826c3531ee0a348ab1d5
marekquarda <mquarda@centrum.cz>
2024-11-09 13:46:34 +0100

    asdfa

--- lcd/src/main.cpp
+++ lcd/src/main.cpp
@@ -21,7 +21,7 @@ int main(void)
 	//lcd_write_character(1);
 	//lcd_write_character('j');
 	//lcd_write_word("Hello, World!");
-	lcd_goto_xy(0,1);
+	lcd_goto_xy(1,1);
 	//lcd_write_character(1);
 	//lcd_write_word("LCD is working");
  
