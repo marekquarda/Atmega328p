17af22093318075ef68a1bfd6be9dacf64e6cbee
marekquarda <mquarda@centrum.cz>
2024-11-09 21:34:11 +0100

    asdfa

--- lcd/src/main.cpp
+++ lcd/src/main.cpp
@@ -17,17 +17,18 @@ int main(void)
 	//lcd_write_word("Hello World");	/* Write string on 2nd line*/
 	while(1) {
 		lcd_clear();
-		lcd_goto_xy(1,0);
-		lcd_write_word("ElectronicWINGS@");	/* Write string on 1st line of LCD*/
-		//lcd_send_command(0xC0);		/* Go to 2nd line*/
-		lcd_goto_xy(0,0);
-		lcd_write_word("ElectronicWINGS@");	/* Write string on 2nd line*/
-		lcd_clear();
-		lcd_goto_xy(1,0);
-		lcd_write_word("8888888888888888");	/* Write string on 1st line of LCD*/
-		lcd_goto_xy(0,0);
-		//lcd_send_command(0xC0);		/* Go to 2nd line*/
-		lcd_write_word("8888888888888888");	/* Write string on 2nd line*/
+		lcd_write_word("ABCDEFGHIJKLMNOPQRSTUV1234567890");
+		// lcd_goto_xy(1,0);
+		// lcd_write_word("ElectronicWINGS@");	/* Write string on 1st line of LCD*/
+		// //lcd_send_command(0xC0);		/* Go to 2nd line*/
+		// lcd_goto_xy(0,0);
+		// lcd_write_word("ElectronicWINGS@");	/* Write string on 2nd line*/
+		// lcd_clear();
+		// lcd_goto_xy(1,0);
+		// lcd_write_word("8888888888888888");	/* Write string on 1st line of LCD*/
+		// lcd_goto_xy(0,0);
+		// //lcd_send_command(0xC0);		/* Go to 2nd line*/
+		// lcd_write_word("8888888888888888");	/* Write string on 2nd line*/
 	};
 }
 
