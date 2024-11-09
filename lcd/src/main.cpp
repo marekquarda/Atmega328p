2fc937f69463af0da90faf16140a653d57213aad
marekquarda <mquarda@centrum.cz>
2024-11-09 18:35:32 +0100

    safd

--- lcd/src/main.cpp
+++ lcd/src/main.cpp
@@ -18,12 +18,12 @@ int main(void)
 	while(1) {
 		lcd_clear();
 		lcd_write_word("ElectronicWINGS@");	/* Write string on 1st line of LCD*/
-		lcd_send_command(0xC0);		/* Go to 2nd line*/
+		//lcd_send_command(0xC0);		/* Go to 2nd line*/
 		lcd_write_word("ElectronicWINGS@");	/* Write string on 2nd line*/
 		lcd_clear();
 		lcd_write_word("8888888888888888");	/* Write string on 1st line of LCD*/
 		//lcd_send_command(0xC0);		/* Go to 2nd line*/
-		lcd_goto_xy(1,0);
+		//lcd_goto_xy(1,0);
 		lcd_write_word("8888888888888888");	/* Write string on 2nd line*/
 	};
 }
