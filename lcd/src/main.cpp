cb7affff43008f652367a39a9bf46ff0eeb18531
marekquarda <mquarda@centrum.cz>
2024-11-09 18:31:41 +0100

    asdfas

--- lcd/src/main.cpp
+++ lcd/src/main.cpp
@@ -21,9 +21,10 @@ int main(void)
 		lcd_send_command(0xC0);		/* Go to 2nd line*/
 		lcd_write_word("Hello World");	/* Write string on 2nd line*/
 		lcd_clear();
-		lcd_write_word("8888888888888888");	/* Write string on 1st line of LCD*/
-		lcd_send_command(0xC0);		/* Go to 2nd line*/
-		lcd_write_word("8888888888888888");	/* Write string on 2nd line*/
+		lcd_write_word("88888888888888888");	/* Write string on 1st line of LCD*/
+		//lcd_send_command(0xC0);		/* Go to 2nd line*/
+		lcd_goto_xy(1,0);
+		lcd_write_word("88888888888888888");	/* Write string on 2nd line*/
 	};
 }
 
