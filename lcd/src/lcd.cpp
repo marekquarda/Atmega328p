793cb67ddb936aaaf467e5c33f1b84b151679150
marekquarda <mquarda@centrum.cz>
2024-11-09 23:40:44 +0100

    sdsd

--- lcd/src/lcd.cpp
+++ lcd/src/lcd.cpp
@@ -29,6 +29,8 @@ void lcd_init(void)
 	// lcd_send_command(0x30);		
 	// _delay_ms(20);	
 	/* LCD Power ON delay always >15ms */
+	lcd_send_command(0x33);
+	lcd_send_command(0x32);	/* Send for 4 bit initialization of LCD  */
 	lcd_send_command(LCD_FUNCTION_4BIT_2LINES);     /* Use 2 line and initialize 5*7 matrix in (4-bit mode)*/
 	lcd_send_command(LCD_DISP_ON);    				/* Display on cursor off*/
 	lcd_send_command(LCD_CMD_CURSOR_HOME);		    /* send for 4 bit initialization of LCD  */
