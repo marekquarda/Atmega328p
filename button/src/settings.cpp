2923b8cfdd475610fcc096825f78f39c4176095e
marekquarda <mquarda@centrum.cz>
2024-11-13 00:00:08 +0100

    llll

--- button/src/settings.cpp
+++ button/src/settings.cpp
@@ -60,9 +60,9 @@ void initInterruptSettings(void) {
 }
 
 void shortButtonUp() {
-    PORTB = 0b00000000;
+  //  PORTB = 0b00000000;
   // _delay_ms(500);
-  // PORTB = 0b00100000;
+   PORTB = 0b00100000;
   // _delay_ms(500);
 }
 
