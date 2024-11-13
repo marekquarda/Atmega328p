29cb9664ebe27b9133207db36b837ab74b66da69
marekquarda <mquarda@centrum.cz>
2024-11-13 01:14:51 +0100

    llllll

--- button/src/settings.cpp
+++ button/src/settings.cpp
@@ -55,8 +55,10 @@ void initInterruptSettings(void) {
     PORTC = 0b00000101;
 
     // Enable PCMSK1 (Group 1: PCINT8 to PCINT14)
-    PCICR |= 0b00000010;
-    PCMSK1 |= 0b0000001;
+    PCICR |= _BV(PCIE1);
+    //PCICR |= 0b00000010;
+    PCMSK1 |= _BV(PCINT1);
+    //PCMSK1 |= 0b0000001;
     // Button Setting
     // red led
     DDRD |=  0b00000010;
