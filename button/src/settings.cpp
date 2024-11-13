a746fb2d697c5b72399a9f9e7b5fb327c3ed24bf
marekquarda <mquarda@centrum.cz>
2024-11-13 22:19:33 +0100

    fgfg

--- button/src/settings.cpp
+++ button/src/settings.cpp
@@ -18,7 +18,7 @@ ISR(PCINT1_vect) {
     // Pins C0 - C7 
     // Button Up
     if ((PINC & (1<<PINC0))==0) {
-        lcd_write_word("Pin C0 ...");
+        lcd_write_word("Pin C0,");
         while ((bit_is_clear(PINC, PC0))) { // button hold down
             timer++;
             _delay_ms(1);
@@ -35,7 +35,7 @@ ISR(PCINT1_vect) {
     }
     // Button Set
     if ((PINC & (1<<PINC2))== 0) {
-        lcd_write_word("Pin C2 ...");
+        lcd_write_word("Pin C2,");
         while ((bit_is_clear(PINC, PC2))) { // button hold down
             timer++;
             _delay_ms(1);
