028d73489cf6e9f6546fe175c89c83d4c0e6eaab
marekquarda <mquarda@centrum.cz>
2024-11-15 16:05:46 +0100

    eeprom

---
+++ eeprom/src/pins.h
@@ -0,0 +1,30 @@
+/*
+ * pin_config.h
+ *
+ * Author : Hossam Elbahrawy
+ */ 
+#ifndef PIN_CONFIG_H_
+#define PIN_CONFIG_H_
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+#define DATA_BUS	PORTB
+#define CTL_BUS		PORTD
+#define DATA_DDR	DDRB
+#define CTL_DDR		DDRD
+// define port B
+#define LCD_D4			PB0  // 0
+#define LCD_D5			PB1  // 1
+#define LCD_D6			PB2  // 2 
+#define LCD_D7			PB3  // 3
+#define LCD_VCC         PB4  // 4
+// define port D
+#define PWR_C           PD3  // 3
+#define LCD_K           PD5  // 5
+
+//#define	LCD_RW	    // GND
+#define	LCD_RS			PD6  // 6
+#define LCD_EN			PD7  // 7
+
+#endif /* PIN_CONFIG_H_ */
