45467d88a29b63d750f2dcff942dc49fade31b0b
marekquarda <mquarda@centrum.cz>
2024-11-26 18:03:38 +0100

    asdf

--- button/src/settings.cpp
+++ button/src/settings.cpp
@@ -11,7 +11,7 @@
 // ISR for port B
 ISR(PCINT1_vect) {
     cli();
-    lcd_clear();
+    //lcd_clear();
     uint16_t timer = 0;
     // Execute instruction for PCINT8 to PCINT14
     // Pins C0 - C7 
@@ -74,11 +74,13 @@ void initInterruptSettings(void) {
 }
 
 void shortButtonUp() {
+    lcd_clear();
     lcd_write_word("Short Up");
    // _delay_ms(2000);
 }
 
 void longButtonUp() {
+    lcd_clear();
     lcd_write_word("Long Up");
    // _delay_ms(2000);
 }
