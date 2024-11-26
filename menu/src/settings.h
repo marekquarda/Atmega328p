31ae4659963268c4943be61fc3a7c7c9d848df52
marekquarda <mquarda@centrum.cz>
2024-11-26 18:15:50 +0100

    Base for Menu

---
+++ menu/src/settings.h
@@ -0,0 +1,26 @@
+/*
+ * settings.h
+ *
+ * Created: 2024-11-01 20:00:32
+ * Author : Marek Xara Quarda
+ */ 
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+
+
+#ifndef SETTINGS_H_
+#define SETTINGS_H_
+
+#define BTN_DEBOUCE 20 //ms
+
+//bool btnUp = false;
+//bool btnDown = false;
+
+void initInterruptSettings(void);
+void shortButtonUp(void);
+void longButtonUp(void);
+void shortButtonSet(void);
+void longButtonSet(void);
+
+#endif // SETTINGS_H_
