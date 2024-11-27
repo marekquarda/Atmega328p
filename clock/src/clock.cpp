cbe96dd25f83230311beb2f8d06a46478f149089
marekquarda <mquarda@centrum.cz>
2024-11-27 17:47:53 +0100

    ssdsd

--- clock/src/clock.cpp
+++ clock/src/clock.cpp
@@ -23,84 +23,84 @@ void TWI_Pullups() {
 
 void TWI_Init()
 {
-	TWI_Pullups();
+//	TWI_Pullups();
 	
 	// DDRC = (1<<INT_CLOCK);
 	// INT_DISABLE;
 	// //About 100kHz for 1.6MHz clock
-	TWBR = 0;										//Set bitrate factor to 0
-	TWSR &= ~((1<<TWPS1) | (1<<TWPS0));				//Set prescaler to 1
-	//twi_init();
+//	TWBR = 0;										//Set bitrate factor to 0
+	//TWSR &= ~((1<<TWPS1) | (1<<TWPS0));				//Set prescaler to 1
+	twi_init();
 }
 
-void TWI_Start()
-{
-	TWCR = (1<<TWINT) | (1<<TWEN) | (1<<TWSTA);
-	while (!(TWCR & (1<<TWINT)));
-}
+// void TWI_Start()
+// {
+// 	TWCR = (1<<TWINT) | (1<<TWEN) | (1<<TWSTA);
+// 	while (!(TWCR & (1<<TWINT)));
+// }
 
-void TWI_Stop()
-{
-	TWCR = (1<<TWINT) | (1<<TWEN) | (1<<TWSTO);
-	while ((TWCR & (1<<TWSTO)));
-}
+// void TWI_Stop()
+// {
+// 	TWCR = (1<<TWINT) | (1<<TWEN) | (1<<TWSTO);
+// 	while ((TWCR & (1<<TWSTO)));
+// }
 
-uint8_t TWI_Read(uint8_t ack)
-{
-	TWCR = (1<<TWINT) | (1<<TWEN) | (((ack ? 1 : 0)<<TWEA));
-	while (!(TWCR & (1<<TWINT)));
-	return TWDR;
-}
+// uint8_t TWI_Read(uint8_t ack)
+// {
+// 	TWCR = (1<<TWINT) | (1<<TWEN) | (((ack ? 1 : 0)<<TWEA));
+// 	while (!(TWCR & (1<<TWINT)));
+// 	return TWDR;
+// }
 
-void TWI_Write(uint8_t byte)
-{
-	TWDR = byte;
-	TWCR = (1<<TWINT) | (1<<TWEN);
-	while (!(TWCR & (1<<TWINT)));
-}
+// void TWI_Write(uint8_t byte)
+// {
+// 	TWDR = byte;
+// 	TWCR = (1<<TWINT) | (1<<TWEN);
+// 	while (!(TWCR & (1<<TWINT)));
+// }
 
 
 //COMMUNICATION LAYER
 
 void PCF_Write(uint8_t addr, uint8_t *data, uint8_t count) {
-//	twi_wire(PCF8563_WRITE_ADDR, addr, data, count);
-	TWI_Start();
+	twi_wire(PCF8563_WRITE_ADDR, addr, data, count);
+//	TWI_Start();
 
-	TWI_Write(PCF8563_WRITE_ADDR);
-	TWI_Write(addr);
+	// TWI_Write(PCF8563_WRITE_ADDR);
+	// TWI_Write(addr);
 
-	while (count) {
-		count--;
+	// while (count) {
+	// 	count--;
 
-		TWI_Write(*data);
-		data++;
-	}
+	// 	TWI_Write(*data);
+	// 	data++;
+	// }
 
-	TWI_Stop();
+	// TWI_Stop();
 
 }
 
 void PCF_Read(uint8_t addr, uint8_t *data, uint8_t count) {
-	TWI_Start();
+	// TWI_Start();
 
-	TWI_Write(PCF8563_WRITE_ADDR);
-	TWI_Write(addr);
+	// TWI_Write(PCF8563_WRITE_ADDR);
+	// TWI_Write(addr);
 
-	TWI_Stop();
-	TWI_Start();
+	// TWI_Stop();
+	// TWI_Start();
 
-	TWI_Write(PCF8563_READ_ADDR);
+	// TWI_Write(PCF8563_READ_ADDR);
 
-	while (count)
-	{
-		count--;
+	// while (count)
+	// {
+	// 	count--;
 
-		*data = TWI_Read(count);
-		data++;
-	}
+	// 	*data = TWI_Read(count);
+	// 	data++;
+	// }
 
-	TWI_Stop();
-	//twi_read(PCF8563_READ_ADDR, addr, data, count);
+	// TWI_Stop();
+	twi_read(PCF8563_READ_ADDR, addr, data, count);
 }
 
 
@@ -110,82 +110,82 @@ void PCF_Read(uint8_t addr, uint8_t *data, uint8_t count) {
 #define BinToBCD(bin) ((((bin) / 10) << 4) + ((bin) % 10))
 
 
-void PCF_Init(uint8_t mode)
+ void PCF_Init(uint8_t mode)
 {
 	TWI_Init();
 
-	 uint8_t tmp = 0b00000000;
-	 PCF_Write(0x00, &tmp, 1);				//Control_status_1
+	//  uint8_t tmp = 0b00000000;
+	//  PCF_Write(0x00, &tmp, 1);				//Control_status_1
 
-	 mode &= 0b00010011;						//Mask unnecessary bits
-	 PCF_Write(0x01, &mode, 1);				//Control_status_2
+	//  mode &= 0b00010011;						//Mask unnecessary bits
+	//  PCF_Write(0x01, &mode, 1);				//Control_status_2
 }
 
-uint8_t PCF_GetAndClearFlags()
-{
-	uint8_t flags;
-	PCF_Read(0x01, &flags, 1);				//Control_status_2
+// uint8_t PCF_GetAndClearFlags()
+// {
+// 	uint8_t flags;
+// 	PCF_Read(0x01, &flags, 1);				//Control_status_2
 
-	uint8_t cleared = flags & 0b00010011;	//Mask only configuration bits
-	PCF_Write(0x01, &cleared, 1);			//Control_status_2
+// 	uint8_t cleared = flags & 0b00010011;	//Mask only configuration bits
+// 	PCF_Write(0x01, &cleared, 1);			//Control_status_2
 
-	return flags & 0x0C;					//Mask unnecessary bits
-}
+// 	return flags & 0x0C;					//Mask unnecessary bits
+// }
 
-void PCF_SetClockOut(uint8_t mode)
-{
-	mode &= 0b10000011;
-	PCF_Write(0x0D, &mode, 1);				//CLKOUT_control
-}
+// void PCF_SetClockOut(uint8_t mode)
+// {
+// 	mode &= 0b10000011;
+// 	PCF_Write(0x0D, &mode, 1);				//CLKOUT_control
+// }
 
-void PCF_SetTimer(uint8_t mode, uint8_t count)
-{
-	mode &= 0b10000011;
-	PCF_Write(0x0E, &mode, 1);				//Timer_control
+// void PCF_SetTimer(uint8_t mode, uint8_t count)
+// {
+// 	mode &= 0b10000011;
+// 	PCF_Write(0x0E, &mode, 1);				//Timer_control
 
-	PCF_Write(0x0F, &count, 1);				//Timer
-}
+// 	PCF_Write(0x0F, &count, 1);				//Timer
+// }
 
-uint8_t PCF_GetTimer()
-{
-	uint8_t count;
-	PCF_Read(0x0F, &count, 1);				//Timer
+// uint8_t PCF_GetTimer()
+// {
+// 	uint8_t count;
+// 	PCF_Read(0x0F, &count, 1);				//Timer
 
-	return count;
-}
+// 	return count;
+// }
 
-uint8_t PCF_SetAlarm(PCF_Alarm *alarm)
-{
-	if ((alarm->minute >= 60 && alarm->minute != 80) || (alarm->hour >= 24 && alarm->hour != 80) || (alarm->day > 32 && alarm->day != 80) || (alarm->weekday > 6 && alarm->weekday != 80))
-	{
-		return 1;
-	}
+// uint8_t PCF_SetAlarm(PCF_Alarm *alarm)
+// {
+// 	if ((alarm->minute >= 60 && alarm->minute != 80) || (alarm->hour >= 24 && alarm->hour != 80) || (alarm->day > 32 && alarm->day != 80) || (alarm->weekday > 6 && alarm->weekday != 80))
+// 	{
+// 		return 1;
+// 	}
 
-	uint8_t buffer[4];
+// 	uint8_t buffer[4];
 
-	buffer[0] = BinToBCD(alarm->minute) & 0xFF;
-	buffer[1] = BinToBCD(alarm->hour) & 0xBF;
-	buffer[2] = BinToBCD(alarm->day) & 0xBF;
-	buffer[3] = BinToBCD(alarm->weekday) & 0x87;
+// 	buffer[0] = BinToBCD(alarm->minute) & 0xFF;
+// 	buffer[1] = BinToBCD(alarm->hour) & 0xBF;
+// 	buffer[2] = BinToBCD(alarm->day) & 0xBF;
+// 	buffer[3] = BinToBCD(alarm->weekday) & 0x87;
 
-	PCF_Write(0x09, buffer, sizeof(buffer));
+// 	PCF_Write(0x09, buffer, sizeof(buffer));
 
-	return 0;
-}
+// 	return 0;
+// }
 
-uint8_t PCF_GetAlarm(PCF_Alarm *alarm)
-{
-	uint8_t buffer[4];
+// uint8_t PCF_GetAlarm(PCF_Alarm *alarm)
+// {
+// 	uint8_t buffer[4];
 
-	PCF_Read(0x09, buffer, sizeof(buffer));
+// 	PCF_Read(0x09, buffer, sizeof(buffer));
 
-	alarm->minute = (((buffer[0] >> 4) & 0x0F) * 10) + (buffer[0] & 0x0F);
-	alarm->hour = (((buffer[1] >> 4) & 0x0B) * 10) + (buffer[1] & 0x0F);
-	alarm->day = (((buffer[2] >> 4) & 0x0B) * 10) + (buffer[2] & 0x0F);
-	alarm->weekday = (((buffer[3] >> 4) & 0x08) * 10) + (buffer[3] & 0x07);
+// 	alarm->minute = (((buffer[0] >> 4) & 0x0F) * 10) + (buffer[0] & 0x0F);
+// 	alarm->hour = (((buffer[1] >> 4) & 0x0B) * 10) + (buffer[1] & 0x0F);
+// 	alarm->day = (((buffer[2] >> 4) & 0x0B) * 10) + (buffer[2] & 0x0F);
+// 	alarm->weekday = (((buffer[3] >> 4) & 0x08) * 10) + (buffer[3] & 0x07);
 
-	return 0;
-}
+// 	return 0;
+// }
 
 uint8_t PCF_SetDateTime(PCF_DateTime *dateTime)
 {
