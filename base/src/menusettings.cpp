b5c78c03ef41eca9c913bf93873e7fd823d38673
marekquarda <mquarda@centrum.cz>
2024-12-04 23:38:27 +0100

    sd

--- base/src/menusettings.cpp
+++ base/src/menusettings.cpp
@@ -160,12 +160,12 @@ MENU_ITEM(Menu_3, Menu_1, Menu_2, NULL_MENU, Menu_2_1, Current_Select, NULL, "3"
 //MENU_ITEM(Menu_4, Menu_1, NULL_MENU, NULL_MENU, NULL_MENU, Current_Select,Current_Enter, "4");
 
 // Voltage SubMenu initialization
-MENU_ITEM(Menu_1_1, Menu_1_2, Menu_1_1, Menu_2, NULL_MENU, Voltage_Setting_Up, NULL, "1.1");
-MENU_ITEM(Menu_1_2, Menu_1_2, Menu_1_1, Menu_2, NULL_MENU, Voltage_Setting_Down, NULL, "1.2");
+MENU_ITEM(Menu_1_1, Menu_1_2, Menu_1_1, Menu_2, Menu_SV, Voltage_Setting_Up, NULL, "1.1");
+MENU_ITEM(Menu_1_2, Menu_1_2, Menu_1_1, Menu_2, Menu_SV, Voltage_Setting_Down, NULL, "1.2");
 
 // Current SubMenu initialization
-MENU_ITEM(Menu_2_1, Menu_2_2, Menu_2_1, Menu_3, NULL_MENU, Current_Setting_Up, NULL, "2.1");
-MENU_ITEM(Menu_2_2, Menu_2_2, Menu_2_1, Menu_3, NULL_MENU, Current_Setting_Down, NULL, "2.2");
+MENU_ITEM(Menu_2_1, Menu_2_2, Menu_2_1, Menu_3, Menu_SC, Current_Setting_Up, NULL, "2.1");
+MENU_ITEM(Menu_2_2, Menu_2_2, Menu_2_1, Menu_3, Menu_SC, Current_Setting_Down, NULL, "2.2");
 
 MENU_ITEM(Menu_SC, MENU_NUL, MENU_NULL, NULL_MENU, Menu_3, Current_Save, NULL,"3.0");
 MENU_ITEM(Menu_SV, MENU_NUL, MENU_NULL, NULL_MENU, Menu_2, Voltage_Save,NULL, "3.0");
