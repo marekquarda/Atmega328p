214fe0cfa1448e14dc59552a955c552d12051edb
marekquarda <mquarda@centrum.cz>
2024-12-07 00:59:04 +0100

    asdf

--- dac/src/i2cmaster.cpp
+++ dac/src/i2cmaster.cpp
@@ -72,9 +72,9 @@ void i2c_start_wait(unsigned char address)
         // wait until transmissin complete
         while (!(TWCR & (1<<TWINT)));
         
-        // // check value of TWI Status Register. Mask prescaler bits.
-        // twst = TW_STATUS & 0xF8;
-        // if((twst != TW_START) && (twst != TW_REP_START)) continue;
+        // check value of TWI Status Register. Mask prescaler bits.
+        twst = TW_STATUS & 0xF8;
+        if((twst != TW_START) && (twst != TW_REP_START)) continue;
         
         // send device address
         TWDR = address;
@@ -83,18 +83,18 @@ void i2c_start_wait(unsigned char address)
         // wait until transmitioon completed
         while (!(TWCR & (1<<TWINT)));
         
-        // check value of TWI status register. Mask prescaler bits.
-        // twst = TW_STATUS & 0xF8;
-        // if ((twst == TW_MT_SLA_NACK) || (twst == TW_MR_DATA_NACK))
-        // {
-        //     /* device busy, send stop condition to terminate write operation */
-        //     TWCR = (1<<TWINT) | (1<<TWEN) | (1<<TWSTO);
+        check value of TWI status register. Mask prescaler bits.
+        twst = TW_STATUS & 0xF8;
+        if ((twst == TW_MT_SLA_NACK) || (twst == TW_MR_DATA_NACK))
+        {
+            /* device busy, send stop condition to terminate write operation */
+            TWCR = (1<<TWINT) | (1<<TWEN) | (1<<TWSTO);
 
-        //     // wait until stop condition is executed and bus released
-        //     while (TWCR & (1<<TWSTO));
+            // wait until stop condition is executed and bus released
+            while (TWCR & (1<<TWSTO));
             
-        //     continue;
-        // }
+            continue;
+        }
         break;
     }
 }
@@ -142,9 +142,9 @@ unsigned char i2c_write(uint8_t data)
     // wait until transmission completed
     while (!(TWCR & (1<<TWINT)));
     
-    // // check value of TWI status register. Mask prescaler bits
-    // twst = TW_STATUS & 0xF8;
-    // if(twst != TW_MT_DATA_ACK) return 1;
+    // check value of TWI status register. Mask prescaler bits
+    twst = TW_STATUS & 0xF8;
+    if(twst != TW_MT_DATA_ACK) return 1;
     return 0;
 }
 
