2cb74b930286beb1dc67f3b400b84f612f8ddff9
marekquarda <mquarda@centrum.cz>
2024-12-07 22:39:35 +0100

    ddf

--- dac/src/menusettings.cpp
+++ dac/src/menusettings.cpp
@@ -173,8 +173,8 @@ MENU_ITEM(Menu_1_2, Menu_1_2, Menu_1_1, Menu_SV, NULL_MENU, Voltage_Setting_Down
 MENU_ITEM(Menu_2_1, Menu_2_2, Menu_2_1, Menu_SC, NULL_MENU, Current_Setting_Up, NULL, "2.1");
 MENU_ITEM(Menu_2_2, Menu_2_2, Menu_2_1, Menu_SC, NULL_MENU, Current_Setting_Down, NULL, "2.2");
 
-MENU_ITEM(Menu_SC, NULL_MENU, NULL_MENU, Menu_3, NULL_MENU, Current_Save, NULL,"3.0");
-MENU_ITEM(Menu_SV, NULL_MENU, NULL_MENU, Menu_2, NULL_MENU, Voltage_Save, NULL, "3.0");
+MENU_ITEM(Menu_SC, NULL_MENU, NULL_MENU, NULL_MENU, NULL_MENU, Current_Save, NULL,"3.0");
+MENU_ITEM(Menu_SV, NULL_MENU, NULL_MENU, NULL_MENU, NULL_MENU, Voltage_Save, NULL, "3.0");
 
 void InitMenu() {
     // Init menu
