023fb12e53331a15fd27d597664641e3015b90eb
marekquarda <mquarda@centrum.cz>
2024-12-08 00:46:51 +0100

    sadf

--- dac/src/main.cpp
+++ dac/src/main.cpp
@@ -40,7 +40,7 @@ int main(void)
 	sei();          // Global Interrupts
 
 	// set dac default
-	//dac_setvoltage(DAC_GND_ADDRESS, 1, 5, 1);
+	dac_setvoltage(DAC_GND_ADDRESS, 1, 5, 1);
 	//dac_setvoltage(DAC_VCC_ADDRESS, 2, 5, 1);
 
 	while (1)
