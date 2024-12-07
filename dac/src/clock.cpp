f70992ca3bf8b8ba1c4c7e55e1d94dd1d303b1f6
marekquarda <mquarda@centrum.cz>
2024-12-07 01:17:38 +0100

    asdf

--- dac/src/clock.cpp
+++ dac/src/clock.cpp
@@ -57,7 +57,7 @@ void PCF_Write(uint8_t addr, uint8_t *data, uint8_t count) {
 }
 
 void PCF_Read(uint8_t addr, uint8_t *data, uint8_t count) {
-	i2c_start(PCF8563_WRITE_ADDR);
+	i2c_start(PCF8563_READ_ADDR);
 	i2c_write(addr);
 
 	i2c_stop();
