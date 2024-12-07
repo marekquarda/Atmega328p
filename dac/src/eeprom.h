71330d592bda7089a76d9a07b61efa8dccedf457
marekquarda <mquarda@centrum.cz>
2024-12-07 22:30:43 +0100

    fdfd

--- dac/src/eeprom.h
+++ dac/src/eeprom.h
@@ -33,7 +33,7 @@ enum{
 };
 
 uint8_t EEPROM_read(uint16_t uiAddress, uint8_t* data);
-uint8_t EEPROM_write(uint16_t uiAddress, uint8_t* data);
+uint8_t EEPROM_write(uint16_t uiAddress, uint8_t ucData);
 uint8_t EEPROM_update(uint16_t uiAddress, uint8_t ucData);
 uint8_t EEPROM_update_batch(uint16_t uiAdress, void *data, uint16_t len);
 uint8_t EEPROM_read_batch(uint16_t uiAdress, void *data, uint16_t len);
