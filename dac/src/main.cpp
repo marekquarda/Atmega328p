ec1353479d7b2d4d7fd09412f0600e8d9995f527
marekquarda <mquarda@centrum.cz>
2024-12-08 17:34:09 +0100

    adsf

--- dac/src/main.cpp
+++ dac/src/main.cpp
@@ -50,9 +50,8 @@ int main(void)
 		for(uint8_t i =0; i < 256; i++) {
 			uint8_t sinval = sine256[i];
 			dac_setvoltagefast(DAC_GND_ADDRESS_WRITE, sinval, 255);
+			_delay_ms(5);
 			dac_setvoltagefast(DAC_VCC_ADDRESS_WRITE, sinval, 255);	
-			_delay_ms(2);
-			
 		}
 	}
 
