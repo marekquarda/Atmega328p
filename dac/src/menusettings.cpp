8ba363df0e9a6aa503cf74a1deb9738a665b76a2
marekquarda <mquarda@centrum.cz>
2024-12-10 18:33:21 +0100

    asdf

--- dac/src/menusettings.cpp
+++ dac/src/menusettings.cpp
@@ -46,7 +46,8 @@ static void Voltage_Select(void)
 static void Current_Select(void)
 {
     viewTimeDate(SHOW_NONE);
-    LCDstring("Cuurrre ... ");
+    //LCDstring("Cuurrre ... ");
+    printValue(PRINT_CURRENT);
     EEPROM_read_batch(CURRENT_ADDR, &current, 2);
     //printValue(PRINT_CURRENT);
 }
